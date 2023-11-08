f0bb422fc5726560bce8d342cb19b5fac3b2f9e9
kailaanderson <142853482+kailaanderson@users.noreply.github.com>
2023-11-07 21:18:36 -0500

    Update consumer.c

--- consumer.c
+++ consumer.c
@@ -4,18 +4,24 @@
 
 #include "sharedMem.h"
 
-int main()
-{
+void* consume(){
   while(1){
-    int consume;
+    int consume; //where to hold the consumed number
 
     sem_wait(&Full); // wait untill buffer is full
-    pthread_mutex_lock(&mutex);
-    consume = buffer[count-1];
-    count--;
-    pthread_mutex_unlock(&mutex);
-    sem_post(&Empty);
-
-    printf("Consumed %d\n", consume);
+    pthread_mutex_lock(&mutex); //lock access to the buffer
+    for(int i = 0; i < 2; ++i){
+        consume = buffer[i]; //consume each element and output the number that was consumed 
+        printf("Consumed: ");
+        printf("%d", consume);
+    }
+    pthread_mutex_unlock(&mutex); //unlock access
+    sem_post(&Empty); //signal that the buffer is empty
   }
+}
+int main()
+{
+  pthread_create(&consumer, NULL, consume, NULL); //create the thread and call the consume function
+  consume();
+  sem_init(&Full, 0, 0); // initialize the semaphore
 }
