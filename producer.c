5a740e09d448a9267828bc99dc60a6ec7b3ae320
kailaanderson <142853482+kailaanderson@users.noreply.github.com>
2023-11-07 21:21:43 -0500

    Update producer.c

--- producer.c
+++ producer.c
@@ -3,15 +3,22 @@
 
 #include "sharedMem.h"
 
-int main() 
-{
-  while(1){
-    int data = rand() % 100; //generate random numeber
-
+void* produce(){
+  while(count > 0){
     sem_wait(&Empty); // wait until empty is true
     pthread_mutex_lock(&mutex); // lock access to thread
-    buffer[count] = data; // put data into buffer
-    count++;
+    for(int i = 0; i < 2; ++i){
+      int data = rand() % 100; //generate random number
+      buffer[i] = data; //put random number onto data table
+    }
+    count--; 
     pthread_mutex_unlock(&mutex); //unlock access
-    sem_post(&Full); 
+    sem_post(&Full); //signal that buffer is full
+  }
+}
+int main() 
+{
+  pthread_create(&producer, NULL, produce, NULL); //create a thread and call produce functino
+  produce();
+  sem_init(&Empty, 0, 2);    
 }
